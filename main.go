a3c6028438f3dd8d37f4f2015d03f0bb775f8224
Brandon Jordan <brandonjordan124@gmail.com>
2022-11-07 09:12:09 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
+	"runtime"
 )
 
-var tokens []string
+var EOL = "\n"
+var content string
 
 func main() {
+	if runtime.GOOS == "windows" {
+		EOL = "\r\n"
+	}
 	if len(os.Args) > 1 {
-		load_json(os.Args[1])
+		bytes, err := os.ReadFile(os.Args[1])
+		handle(err)
+		content = string(bytes)
+		parse()
+		// fmt.Println(tokens)
+		fmt.Printf("\n")
+		for _, tok := range tokens {
+			fmt.Println(tok)
+		}
+	} else {
+		fmt.Println("USAGE: jsonp [FILE]")
 	}
 }
 
-func load_json(file string) {
-	bytes, err := os.ReadFile(file)
+func handle(err error) {
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	json := string(bytes[:])
-	Lex(json)
-	Parse(tokens)
 }
